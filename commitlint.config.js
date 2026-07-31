0c551119fe914be4a8da088315058ef8c1639d79
Carlos Ruiz <car.ruiz90@gmail.com>
2026-07-31 12:44:49 -0600

    chore(infra): apply prettier formatting to existing files
    Refs: NEXO-14

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,11 +2,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [
-      2,
-      'always',
-      ['feat', 'fix', 'docs', 'test', 'refactor', 'chore', 'perf'],
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'test', 'refactor', 'chore', 'perf']],
     'scope-enum': [
       2,
       'always',
